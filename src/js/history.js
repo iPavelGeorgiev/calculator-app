3aac2ea7cd48100999dfe8399af89d6c587ab851
Pavel Georgiev <ipavelgeorgiev@outlook.com>
2022-12-26 14:45:49 +0200

    Implement add to history functionality

--- src/js/history.js
+++ src/js/history.js
@@ -32,6 +32,26 @@ class History {
     if (!e.target.classList.contains('modal-history')) return;
     this.modalHistorySel.classList.add('hidden');
   }
+
+  addToHistory(previousOperand, currentOperand, operator, result) {
+    if (!previousOperand || !currentOperand || !operator || !result) return;
+    const [previousOperandString, currentOperandString, resultString] = [previousOperand, currentOperand, result].map(
+      (value) => utilities.numberToLocaleString(value, 7)
+    );
+
+    const computationString = `${previousOperandString} ${operator} ${currentOperandString} = ${resultString}`;
+
+    const storedComputations = localStorage.getItem('computations-history');
+    const computations = JSON.parse(storedComputations) || [];
+    computations.unshift(computationString);
+    localStorage.setItem('computations-history', JSON.stringify(computations));
+
+    const p = document.createElement('p');
+    p.classList.add('computation');
+    p.textContent = computationString;
+
+    this.modalHistoryComputationsSel.prepend(p);
+  }
 }
 
 const modalHistorySel = document.querySelector('.modal-history');
