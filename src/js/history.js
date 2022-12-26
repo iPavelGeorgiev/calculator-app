87f91e685867492d2b5d74f38509dc111835cab3
Pavel Georgiev <ipavelgeorgiev@outlook.com>
2022-12-26 14:41:25 +0200

    Implement load initial history functionality

---
+++ src/js/history.js
@@ -0,0 +1,30 @@
+import utilities from './utilities.js';
+
+class History {
+  constructor(modalHistorySel, modalHistoryComputationsSel) {
+    this.modalHistorySel = modalHistorySel;
+    this.modalHistoryComputationsSel = modalHistoryComputationsSel;
+  }
+
+  loadInitialHistory() {
+    const storedComputations = localStorage.getItem('computations-history');
+
+    if (!storedComputations) return;
+
+    const computations = JSON.parse(storedComputations);
+    computations.forEach((computation) => {
+      const p = document.createElement('p');
+      p.classList.add('computation');
+      p.textContent = computation;
+      this.modalHistoryComputationsSel.prepend(p);
+    });
+  }
+}
+
+const modalHistorySel = document.querySelector('.modal-history');
+const modalHistoryComputationsSel = document.querySelector('.modal-history-computations');
+
+const history = new History(modalHistorySel, modalHistoryComputationsSel);
+history.loadInitialHistory();
+
+export default history;
